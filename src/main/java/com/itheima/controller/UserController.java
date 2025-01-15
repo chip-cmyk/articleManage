f3dc65254636aa28b5197902f08b386b15474f7c
qq_64050341 <qq_64050341@noreply.gitcode.com>
2025-01-15 22:30:25 +0800

    feat: register

---
+++ src/main/java/com/itheima/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.itheima.controller;
+
+import com.itheima.pojo.Result;
+import com.itheima.pojo.User;
+import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public Result register(String username, String password) {
+        User user = userService.findByUsername(username);
+        if (user == null) {
+            userService.register(username, password);
+            return Result.success();
+        } else {
+            return Result.error("用户名已被占用");
+        }
+    }
+}
