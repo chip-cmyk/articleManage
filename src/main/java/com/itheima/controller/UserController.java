c615b7600a23db1454536dba0ccc0d2c3c4cec27
chip-cmyk <290806494@qq.com>
2025-01-16 00:18:44 +0800

    feat: login jwt

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -3,6 +3,8 @@ package com.itheima.controller;
 import com.itheima.pojo.Result;
 import com.itheima.pojo.User;
 import com.itheima.service.UserService;
+import com.itheima.utils.JwtUtil;
+import com.itheima.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -28,5 +33,22 @@ public class UserController {
         } else {
             return Result.error("用户名已被占用");
         }
+    }
+
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\S{5,16}$", message = "用户名长度需要5-16位") String username, @Pattern(regexp = "^\\S{5,16}$", message = "密码长度需要5-16位") String password) {
+        User loginUser = userService.findByUsername(username);
+        if (loginUser == null) {
+            return Result.error("用户名错误");
+        }
+        if (Md5Util.getMD5String(password).equals(loginUser.getPassword())) {
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", loginUser.getId());
+            claims.put("username", loginUser.getUsername());
+            String token = JwtUtil.genToken(claims);
+            return Result.success(token);
+        }
+
+        return Result.error("密码错误");
     }
 }
---
+++ src/main/java/com/itheima/interceptors/LoginInterceptor.java
@@ -0,0 +1,28 @@
+package com.itheima.interceptors;
+
+import com.itheima.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Map;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+        System.out.println(token);
+        System.out.println(token);
+        System.out.println(token);
+        System.out.println(token);
+        try {
+            Map<String, Object> claims = JwtUtil.parseToken(token);
+            return true;
+        } catch (Exception e) {
+            response.setStatus(401);
+            return false;
+        }
+    }
+}
