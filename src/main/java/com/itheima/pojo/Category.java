4fbff71a2aa3723311fafb6b0587b398e072d9d5
chip-cmyk <290806494@qq.com>
2025-01-17 20:36:59 +0800

    fix: date format

--- src/main/java/com/itheima/pojo/Category.java
+++ src/main/java/com/itheima/pojo/Category.java
@@ -1,5 +1,6 @@
 package com.itheima.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -18,6 +19,8 @@ public class Category {
     @Pattern(regexp = "^\\S{1,10}$")
     private String categoryAlias;//分类别名
     private Integer createUser;//创建人ID
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;//创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;//更新时间
 }
