a5ddfd632b4168c6e08a375bb66ade09d5369f0f
chip-cmyk <290806494@qq.com>
2025-01-19 11:50:07 +0800

    feat: article list

--- src/main/java/com/itheima/controller/ArticleController.java
+++ src/main/java/com/itheima/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.itheima.controller;
 
 import com.itheima.pojo.Article;
+import com.itheima.pojo.PageBean;
 import com.itheima.pojo.Result;
 import com.itheima.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,25 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @GetMapping("/list")
-    public Result<String> list() {
-        return Result.success("所有文章数据");
-    }
+//    @GetMapping("/list")
+//    public Result<String> list() {
+//        return Result.success("所有文章数据");
+//    }
 
     @PostMapping
     public Result add(@RequestBody @Validated Article article) {
         articleService.add(article);
         return Result.success();
+    }
+
+    @GetMapping
+    public Result<PageBean<Article>> list(
+            Integer pageNum,
+            Integer pageSize,
+            @RequestParam(required = false) Integer categoryId,
+            @RequestParam(required = false) String state
+    ) {
+        PageBean<Article> pageBean = articleService.list(pageNum, pageSize, categoryId, state);
+        return Result.success(pageBean);
     }
 }
--- src/main/java/com/itheima/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/itheima/service/impl/ArticleServiceImpl.java
@@ -1,12 +1,16 @@
 package com.itheima.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.itheima.mapper.ArticleMapper;
 import com.itheima.pojo.Article;
+import com.itheima.pojo.PageBean;
 import com.itheima.service.ArticleService;
 import com.itheima.utils.ThreadLocalUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -20,5 +24,19 @@ public class ArticleServiceImpl implements ArticleService {
         Integer createUser = ((Integer) map.get("id"));
         article.setCreateUser(createUser);
         articleMapper.add(article);
+    }
+
+    @Override
+    public PageBean<Article> list(Integer pageNum, Integer pageSize, Integer categoryId, String state) {
+        PageBean<Article> pageBean = new PageBean<>();
+        PageHelper.startPage(pageNum, pageSize);
+        Map<String, Object> map = ThreadLocalUtil.get();
+        Integer userId = ((Integer) map.get("id"));
+        List<Article> as = articleMapper.list(userId, categoryId, state);
+        Page<Article> page = (Page<Article>) as;
+        pageBean.setTotal(page.getTotal());
+        pageBean.setItems(page.getResult());
+
+        return pageBean;
     }
 }
